aa027036c8694cc6ab0f2b92108f28db100daad5
Karnesh Jain <jain.karnesh@gmail.com>
2022-12-06 23:15:27 -0500

    initiated numpy array variables

--- calc_neival.py
+++ calc_neival.py
@@ -3,20 +3,28 @@ import numpy as np
 
 def calc_neival(c_trans, p_sl, y_node, q_node, lccf, rtpref, tslrisk, dtonei, profmdl, cutflag, totcpcty, totstock,
                 edgechange):
-    pay_noevent = np.zeros((len(c_trans), 1))
-    pay_event = np.zeros((len(c_trans), 1))
-    xpay_noevent = np.zeros((len(c_trans), 1))
-    ypay_noevent = np.zeros((len(c_trans), 1))
-    xpay_event = np.zeros((len(c_trans), 1))
-    ypay_event = np.zeros((len(c_trans), 1))
-    value_noevent = np.zeros((len(c_trans), 1))
-    value_event = np.zeros((len(c_trans), 1))
-    ival_noevent = np.zeros((len(c_trans), 1))
-    ival_event = np.zeros((len(c_trans), 1))
-    dwght_noevent = np.zeros((len(c_trans), 1))
-    dwght_event = np.zeros((len(c_trans), 1))
-    salwght_noevent = np.zeros((len(c_trans), 1))
-    salwght_event = np.zeros((len(c_trans), 1))
-    valuex = np.zeros((len(c_trans), 1))
-    valuey = np.zeros((len(c_trans), 1))
+    pay_noevent = np.zeros(len(c_trans))
+    pay_event = np.zeros(len(c_trans))
+    xpay_noevent = np.zeros(len(c_trans))
+    ypay_noevent = np.zeros(len(c_trans))
+    xpay_event = np.zeros(len(c_trans))
+    ypay_event = np.zeros(len(c_trans))
+    value_noevent = np.zeros(len(c_trans))
+    value_event = np.zeros(len(c_trans))
+    ival_noevent = np.zeros(len(c_trans))
+    ival_event = np.zeros(len(c_trans))
+    dwght_noevent = np.zeros(len(c_trans))
+    dwght_event = np.zeros(len(c_trans))
+    salwght_noevent = np.zeros(len(c_trans))
+    salwght_event = np.zeros(len(c_trans))
+    valuex = np.zeros(len(c_trans))
+    valuey = np.zeros(len(c_trans))
     iset = np.arange(1, len(c_trans) + 1)
+
+    for i in np.arange(1, len(c_trans)+1).reshape(-1):
+        pay_noevent[i] = y_node(i) * q_node(i) - c_trans(i) * q_node(i)  # payoff with no S&L event
+        pay_event[i] = y_node(i) * q_node(i) - c_trans(i) * q_node(i) - y_node(i) * q_node(i)  # payoff with S&L event
+        xpay_noevent[i] = pay_noevent(i)  # payoff for route A with no S&L event
+        xpay_event[i] = pay_event(i)   # payoff for route A with S&L event
+
+    return neipick, neivalue, valuex
