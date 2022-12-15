ed4e95c918213b8c1c52152117bd499fa66f7efb
Karnesh Jain <jain.karnesh@gmail.com>
2022-12-15 16:33:59 -0500

    modified indexes

--- lldistkm.py
+++ lldistkm.py
@@ -4,10 +4,10 @@ import numpy as np
 def lldistkm(latlon1, latlon2):
 
     radius = 6371
-    lat1 = latlon1[:, 1] * np.pi / 180
-    lat2 = latlon2[:, 1] * np.pi / 180
-    lon1 = latlon1[:, 2] * np.pi / 180
-    lon2 = latlon2[:, 2] * np.pi / 180
+    lat1 = latlon1[:, 0] * np.pi / 180
+    lat2 = latlon2[:, 0] * np.pi / 180
+    lon1 = latlon1[:, 1] * np.pi / 180
+    lon2 = latlon2[:, 1] * np.pi / 180
     deltaLat = lat2 - lat1
     deltaLon = lon2 - lon1
     a = np.sin((deltaLat) / 2) ** 2 + np.multiply(np.multiply(np.cos(lat1), np.cos(lat2)), np.sin(deltaLon / 2) ** 2)
