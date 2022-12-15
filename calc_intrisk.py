75adeab6849be182a1a36d7ec35543d541f39e42
Karnesh Jain <jain.karnesh@gmail.com>
2022-12-15 13:16:18 -0500

    removed extra parenthesis

--- calc_intrisk.py
+++ calc_intrisk.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def calc_intrisk(sloccur, t_eff, alpharisk, betarisk, timeweight):
-    slevnt = np.sum(np.multiply(sloccur, np.tile(np.transpose(timeweight ** (t_eff)), (1, len(sloccur[1, :])))), 1)
+    slevnt = np.sum(np.multiply(sloccur, np.tile(np.transpose(timeweight ** t_eff), (1, len(sloccur[1, :])))), 1)
     tmevnt = np.sum(timeweight ** t_eff)
     sl_risk = (slevnt + alpharisk) / (tmevnt + alpharisk + betarisk)
     return sl_risk, slevnt, tmevnt
