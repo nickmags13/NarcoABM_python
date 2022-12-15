303dd7f4c7430079631f87de225faa4cec625506
Karnesh Jain <jain.karnesh@gmail.com>
2022-12-15 13:15:41 -0500

    coded calc_intrisk function

--- calc_intrisk.py
+++ calc_intrisk.py
@@ -1,4 +1,8 @@
 import numpy as np
-from numpy import matlib
+
 
 def calc_intrisk(sloccur, t_eff, alpharisk, betarisk, timeweight):
+    slevnt = np.sum(np.multiply(sloccur, np.tile(np.transpose(timeweight ** (t_eff)), (1, len(sloccur[1, :])))), 1)
+    tmevnt = np.sum(timeweight ** t_eff)
+    sl_risk = (slevnt + alpharisk) / (tmevnt + alpharisk + betarisk)
+    return sl_risk, slevnt, tmevnt
