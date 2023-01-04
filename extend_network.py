dc5821d1c7ab85d2b3fe1657a8a7f70224d83211
Karnesh Jain <jain.karnesh@gmail.com>
2023-01-04 15:29:47 -0500

    added code to expand_network function

--- extend_network.py
+++ extend_network.py
@@ -1,6 +1,16 @@
 import warnings
-warnings.filterwarnings("ignore")
 import numpy as np
+
+warnings.filterwarnings("ignore")
 
 
 def extend_network(nnodes, NodeTable, EdgeTable, Rdptgrid):
+    ext_NodeTable = NodeTable
+    ext_EdgeTable = EdgeTable
+
+    # Add intermediate nodes
+    # Caribbean
+    inn1 = nnodes + 1
+    ext_NodeTable[inn1, 1] = np.array([inn1])
+
+    return ext_NodeTable, ext_EdgeTable
