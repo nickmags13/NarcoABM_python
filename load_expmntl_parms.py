c4852e9306ce2fd2e3558255815f45f34b76a737
Karnesh Jain <jain.karnesh@gmail.com>
2023-01-10 13:55:35 -0600

    started load_expmntl_parms function

---
+++ load_expmntl_parms.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def load_expmntl_parms(ERUNS):
+
+
+    return sl_max, sl_min, baserisk, riskmltplr, startstock, sl_learn, rt_learn, \
+           losslim, prodgrow, targetseize, intcpctymodel, profitmodel, endstock, growthmdl,\
+           timewght, locthink, expandmax, empSLflag, optSLflag, suitflag, extnetflag, rtcap,\
+           basecap, p_sucintcpt
