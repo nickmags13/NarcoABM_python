3c303a0d01eaa577665257f3b724421647134a98
Karnesh Jain <jain.karnesh@gmail.com>
2023-01-17 12:57:26 -0600

    modified element wise power for different size numpy arrays

--- calc_intrisk.py
+++ calc_intrisk.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def calc_intrisk(sloccur, t_eff, alpharisk, betarisk, timeweight):
-    slevnt = np.sum(np.multiply(sloccur, np.tile(np.transpose(timeweight ** t_eff), (1, len(sloccur[1, :])))), 1)
-    tmevnt = np.sum(timeweight ** t_eff)
+    slevnt = np.sum(np.multiply(sloccur, np.tile(np.transpose(np.power(timeweight, t_eff)), (1, len(sloccur[1, :])))), 1)
+    tmevnt = np.sum(np.power (timeweight, t_eff))
     sl_risk = (slevnt + alpharisk) / (tmevnt + alpharisk + betarisk)  # Check for ./ in python and
     # / equivalent to np.divide
     return sl_risk, slevnt, tmevnt
