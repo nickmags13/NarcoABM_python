7ab0425a9338740dc3d1b5b6b43f8e720ee2811c
Karnesh Jain <jain.karnesh@gmail.com>
2023-01-17 17:06:15 -0600

    changed len for numpy array to shape to get correct dimension

--- calc_neival.py
+++ calc_neival.py
@@ -3,31 +3,31 @@ import numpy as np
 
 def calc_neival(c_trans, p_sl, y_node, q_node, lccf, rtpref, tslrisk, dtonei, profmdl, cutflag, totcpcty, totstock,
                 edgechange):
-    pay_noevent = np.zeros(len(c_trans), 1)
-    pay_event = np.zeros(len(c_trans), 1)
-    xpay_noevent = np.zeros(len(c_trans), 1)
-    ypay_noevent = np.zeros(len(c_trans), 1)
-    xpay_event = np.zeros(len(c_trans), 1)
-    ypay_event = np.zeros(len(c_trans), 1)
-    value_noevent = np.zeros(len(c_trans), 1)
-    value_event = np.zeros(len(c_trans), 1)
-    ival_noevent = np.zeros(len(c_trans), 1)
-    ival_event = np.zeros(len(c_trans), 1)
-    dwght_noevent = np.zeros(len(c_trans), 1)
-    dwght_event = np.zeros(len(c_trans), 1)
-    salwght_noevent = np.zeros(len(c_trans), 1)
-    salwght_event = np.zeros(len(c_trans), 1)
-    valuex = np.zeros(len(c_trans), 1)
-    valuey = np.zeros(len(c_trans), 1)
-    iset = np.arange(0, len(c_trans))
+    pay_noevent = np.zeros((c_trans.shape[1], 1))  # len in numpy defaults to 1st dimension and shape provides a tuple
+    pay_event = np.zeros((c_trans.shape[1], 1))
+    xpay_noevent = np.zeros((c_trans.shape[1], 1))
+    ypay_noevent = np.zeros((c_trans.shape[1], 1))
+    xpay_event = np.zeros((c_trans.shape[1], 1))
+    ypay_event = np.zeros((c_trans.shape[1], 1))
+    value_noevent = np.zeros((c_trans.shape[1], 1))
+    value_event = np.zeros((c_trans.shape[1], 1))
+    ival_noevent = np.zeros((c_trans.shape[1], 1))
+    ival_event = np.zeros((c_trans.shape[1], 1))
+    dwght_noevent = np.zeros((c_trans.shape[1], 1))
+    dwght_event = np.zeros((c_trans.shape[1], 1))
+    salwght_noevent = np.zeros((c_trans.shape[1], 1))
+    salwght_event = np.zeros((c_trans.shape[1], 1))
+    valuex = np.zeros((c_trans.shape[1], 1))
+    valuey = np.zeros((c_trans.shape[1], 1))
+    iset = np.arange(0, c_trans.shape[1])
 
-    for i in np.arange(0, len(c_trans)):
+    for i in np.arange(0, c_trans.shape[1]):
         pay_noevent[i] = y_node[i] * q_node[i] - c_trans[i] * q_node[i]  # payoff with no S&L event
         pay_event[i] = y_node[i] * q_node[i] - c_trans[i] * q_node[i] - y_node[i] * q_node[i]  # payoff with S&L event
         xpay_noevent[i] = pay_noevent[i]  # payoff for route A with no S&L event
         xpay_event[i] = pay_event[i]  # payoff for route A with S&L event
 
-    for i in np.arange(0, len(c_trans)):
+    for i in np.arange(0, c_trans.shape[1]):
         inset = np.where(dtonei == dtonei(i))
         ypay_noevent[i] = np.mean(pay_noevent(inset != i))
         ypay_event[i] = np.mean(pay_event(inset != i))
