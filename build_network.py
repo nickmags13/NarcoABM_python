6a46fcc6ec9bba46b7eca52e4fd388fb185968c5
Karnesh Jain <jain.karnesh@gmail.com>
2023-01-23 18:05:38 -0500

    started build_network method

---
+++ build_network.py
@@ -0,0 +1,13 @@
+#################    Build trafficking network    #########################
+
+import numpy as np
+
+
+def build_network(ca_adm0, Rcagrid_cntry, dptgrid, Rdptgrid, LANDSUIT, dptcodes, dptorder, savedState, stock_0):
+    # Set-up producer and end supply nodes
+
+    strow = ca_adm0.shape[0]
+    stcol = ca_adm0.shape[1]
+    edrow = 100
+    edcol = 100
+
