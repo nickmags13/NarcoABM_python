a24696fbf039fbe8827b88becf61847d6f57ca69
Karnesh Jain <jain.karnesh@gmail.com>
2023-02-10 16:21:33 -0500

    intialized variables

--- intrd_tables_batch.py
+++ intrd_tables_batch.py
@@ -5,6 +5,15 @@ import pandas as pd
 
 
 def intrd_tables_batch(FLOW, slsuccess, SLPROB, NodeTable, EdgeTable, t, testflag, erun, mrun, batchrun):
+    Tflow = pd.DataFrame(columns=['End_Node', 'Start_Node', 'IntitFlow', 'DTO'], index=range(1, EdgeTable.shape[0]),
+                         dtype=float)
+    startFLOW = FLOW[:][:][t] + slsuccess[:][:][t]
+    Tintrd = pd.DataFrame(columns=['End_Node', 'Start_Node', 'IntitProb'], index=range(1, EdgeTable.shape[0]),
+                          dtype=float)
 
-    Tflow = pd.DataFrame(columns=['End_Node', 'Start_Node', 'IntitFlow', 'DTO'], index=range(1, EdgeTable))
+    if t == 1:
+        startSLPROB = SLPROB[:][:][0]
+    else:
+        startSLPROB = SLPROB[:][:][t - 2]
+
     return Tflow, Tintrd
