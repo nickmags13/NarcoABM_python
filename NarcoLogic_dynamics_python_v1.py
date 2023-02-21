d264d0f3ff2f73f05e7cfa534c7c7860f40321fe
Karnesh Jain <jain.karnesh@gmail.com>
2023-02-21 15:54:15 -0500

    started function

--- NarcoLogic_dynamics_python_v1.py
+++ NarcoLogic_dynamics_python_v1.py
@@ -3,3 +3,13 @@ Function for executing NarcoLogic dynamics from Python
 """
 
 import numpy as np
+from optimize_interdiction_batch import optimize_interdiction_batch
+from intrd_tables_batch import intrd_tables_batch
+
+
+def NarcoLogic_dynamics_python_v1(t):
+    # Check what variables NarcoLogic_wrksp.mat is loading and add code to load these variables
+
+    intrdct_events, intrdct_nodes = optimize_interdiction_batch(t, ADJ, testflag, erun, mrun, batchrun)
+    slevent[:][:][t] = intrdct_events
+    
