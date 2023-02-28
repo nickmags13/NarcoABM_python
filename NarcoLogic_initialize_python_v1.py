a1a9f5f8c875670f1fabe4e1cfc6250bc3cb5fdb
Karnesh Jain <jain.karnesh@gmail.com>
2023-02-28 12:16:18 -0500

    started NarcoLogic_initialize_python_v1 function

---
+++ NarcoLogic_initialize_python_v1.py
@@ -0,0 +1,10 @@
+# Initialize NarcoLogic for execution from Python #
+# Notes for executing:
+
+
+import numpy as np
+
+
+def NarcoLogic_initialize_python_v1(mr):
+    argout = 'Dummy output'
+    return argout
