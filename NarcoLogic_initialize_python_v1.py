3073b9f5a67f8c532afcafa34078d690002e439a
Karnesh Jain <jain.karnesh@gmail.com>
2023-02-28 12:33:19 -0500

    loaded files

--- NarcoLogic_initialize_python_v1.py
+++ NarcoLogic_initialize_python_v1.py
@@ -5,6 +5,7 @@
 import numpy as np
 import random
 from load_expmntl_parms import load_expmntl_parms
+import scipy
 
 
 def NarcoLogic_initialize_python_v1(mr):
@@ -25,6 +26,18 @@ def NarcoLogic_initialize_python_v1(mr):
     random.seed(mrun)
 
     # load experimental parameters file
-    sl_max,sl_min,baserisk,riskmltplr,startstock,sl_learn,rt_learn,losslim,prodgrow,targetseize,intcpctymodel,profitmodel,endstock,growthmdl,timewght,locthink,expandmax,empSLflag,optSLflag,suitflag,extnetflag,rtcap,basecap,p_sucintcpt = load_expmntl_parms(ERUNS)
+    sl_max, sl_min, baserisk, riskmltplr, startstock, sl_learn, rt_learn, losslim, prodgrow, targetseize, \
+    intcpctymodel, profitmodel, endstock, growthmdl, timewght, locthink, expandmax, empSLflag, optSLflag, suitflag, \
+    extnetflag, rtcap, basecap, p_sucintcpt = load_expmntl_parms(ERUNS)
+
+    # Load landscape files
+    scipy.io.loadmat('coast_dist')   # Check the file format and if it can be changed to non .mat file
+    scipy.io.loadmat('landsuit_file_default')
+
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # @@@@@@@@@@ Agent Attributes @@@@@@@@@@@@
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+    # Interdiction Agent #
 
     return argout
