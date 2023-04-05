8e64722005f2ee20f7c9288b79ce5307ef122737
Karnesh Jain <jain.karnesh@gmail.com>
2023-04-05 17:10:27 -0500

    added subtracting 1 from row and col in Nodetable for Python indexing

--- NarcoLogic_initialize_python_v1.py
+++ NarcoLogic_initialize_python_v1.py
@@ -88,6 +88,8 @@ def NarcoLogic_initialize_python_v1(mr):
     EdgeTable['EndNodes'] = EdgeTable[['EndNodes_1', 'EndNodes_2']].values.tolist()
     EdgeTable = EdgeTable.drop(columns=['EndNodes_1', 'EndNodes_2'])
     NodeTable = pd.read_csv('data/NodeTable.csv')
+    NodeTable['Row'] = NodeTable['Row'] - 1
+    NodeTable['Col'] = NodeTable['Col'] - 1
     EdgeTable['Capacity'] = rtcap[0, erun] * np.ones(EdgeTable.shape[0])
     nnodes = NodeTable.shape[0]
     mexnode = nnodes
