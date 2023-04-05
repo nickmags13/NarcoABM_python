corrected initial DTO calculation

NarcoLogic_initialize_python_v1.py
@@ -88,6 +88,8 @@ def NarcoLogic_initialize_python_v1(mr):
     EdgeTable['EndNodes'] = EdgeTable[['EndNodes_1', 'EndNodes_2']].values.tolist()
     EdgeTable = EdgeTable.drop(columns=['EndNodes_1', 'EndNodes_2'])
     NodeTable = pd.read_csv('data/NodeTable.csv')
+    NodeTable['Row'] = NodeTable['Row'] - 1
+    NodeTable['Col'] = NodeTable['Col'] - 1
     EdgeTable['Capacity'] = rtcap[0, erun] * np.ones(EdgeTable.shape[0])
     nnodes = NodeTable.shape[0]
     mexnode = nnodes
@@ -103,13 +105,13 @@ def NarcoLogic_initialize_python_v1(mr):
     # CHECK the variable assignments in the for loop ####
     for nn in range(1, nnodes - 2):
         westdir = NodeTable['Col'][nn] - np.where(np.isnan(dcoast[NodeTable['Row'][nn],
-                                                                  np.arange(0, NodeTable['Col'][nn] - 2)]) == 1)[-1]
+                                                                  np.arange(0, NodeTable['Col'][nn] - 2)]) == 1)[0][-1]
         eastdir = np.where(np.isnan(dcoast[NodeTable['Row'][nn], np.arange(NodeTable['Col'][nn] + 1,
-                                                                           LANDSUIT.shape[1] + 1)]) == 1)[0]
+                                                                           LANDSUIT.shape[1] + 1)]) == 1)[0][0]
         northdir = NodeTable['Row'][nn] - np.where(np.isnan(dcoast[np.arange(0, NodeTable['Row'][nn] - 2),
-                                                                   NodeTable['Col'][nn]]) == 1)[-1]
+                                                                   NodeTable['Col'][nn]]) == 1)[0][-1]
         southdir = np.where(np.isnan(dcoast[np.arange(NodeTable['Row'][nn] + 1, LANDSUIT.shape[0] + 1),
-                                            NodeTable['Col'][nn]]) == 1)[0]
+                                            NodeTable['Col'][nn]]) == 1)[0][0]
         mindist, imindist = np.amin(np.array([westdir, eastdir, northdir, southdir]))
         if westdir < 2.5 * eastdir:
             NodeTable['DTO'][nn] = 1