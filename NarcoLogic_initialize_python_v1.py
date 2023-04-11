corrected Node attribute calculation

NarcoLogic_initialize_python_v1.py
@@ -180,16 +180,21 @@ def NarcoLogic_initialize_python_v1(mr):
         # Create adjacency matrix
         ADJ[k, EdgeTable['EndNodes'].str[1][np.where(EdgeTable['EndNodes'].str[0] == k)[0]]] = 1
 
-    breakpoint()
     # Node Attributes
-    remotefac = np.array([[0], [1 - NodeTable['PopSuit'][np.arange(1, nnodes + 1)]]])
-    brdrfac = np.array([[0], [NodeTable['DistBorderSuit'][np.arange(1, nnodes + 1)]]])
-    suitfac = np.array([[0], [NodeTable['LandSuit'][np.arange(1, nnodes + 1)]]])
-    coastfac = np.array([[0], [NodeTable['CoastDist'][np.arange(1, nnodes + 1)] / np.amax(NodeTable.CoastDist)]])
-    nwvec = np.sqrt(np.multiply(0.9, NodeTable['Lat'][np.arange(1, nnodes + 1)] ** 2) +
-                    np.multiply(0.1, NodeTable['Lon'][np.arange(1, nnodes + 1)] ** 2))
-    latfac = np.array([[0], [1 - nwvec / np.amax(nwvec)]])
+    remotefac = (1 - NodeTable['PopSuit'].to_numpy()).reshape(163, 1)
+    remotefac[0, 0] = 0
+    brdrfac = NodeTable['DistBorderSuit'].to_numpy().reshape(163, 1)
+    brdrfac[0, 0] = 0
+    suitfac = NodeTable['LandSuit'].to_numpy().reshape(163, 1)
+    suitfac[0, 0] = 0
+    coastfac = (NodeTable['CoastDist'].to_numpy()/np.amax(NodeTable['CoastDist'])).reshape(163, 1)
+    coastfac[0, 0] = 0
+    nwvec = (np.sqrt(np.multiply(0.9, NodeTable['Lat'].to_numpy() ** 2) +
+                    np.multiply(0.1, NodeTable['Lon'].to_numpy() ** 2))).reshape(163, 1)
+    latfac = 1 - nwvec / np.amax(nwvec)
+    latfac[0, 0] = 0
 
+    breakpoint()
     # Create adjacency matrix
     iendnode = NodeTable['ID'][NodeTable['DeptCode'] == 2]
     ADJ[EdgeTable['EndNodes'].str[0][np.where(EdgeTable['EndNodes'].str[1] == iendnode)[0]], iendnode] = 1