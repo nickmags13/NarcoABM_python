corrected latlon1 calculation

NarcoLogic_initialize_python_v1.py
@@ -205,14 +205,14 @@ def NarcoLogic_initialize_python_v1(mr):
         WGHT[0, np.where(ADJ[j, :] == 1)[0]] = EdgeTable['Weight'][np.where(ADJ[j, :] == 1)[0]]
         CPCTY[j, np.where(ADJ[j, :] == 1)[0]] = EdgeTable['Capacity'][np.where(ADJ[j, :] == 1)[0]]
         # Create distance (in km) matrix
-        breakpoint()
         rows = [int(x) for x in range(len(ADJ[j, :])) if ADJ[j, x] == 1]
         latlon2 = NodeTable.loc[rows, ['Lat', 'Lon']].to_numpy()
 
-        latlon1 = np.tile(np.array([NodeTable['Lat'][j], NodeTable['Lon'][j]]), (len(latlon2[:, 1]), 1))
+        latlon1 = np.tile(NodeTable.loc[j, ['Lat', 'Lon']].to_numpy(), (len(latlon2[:, 1]), 1))
         d1km, d2km = lldistkm(latlon1, latlon2)
         DIST[j, np.where(ADJ[j, :] == 1)[0]] = d1km
 
+        breakpoint()
         # Create added value matrix (USD) and price per node
         ADDVAL[j, np.where(ADJ[j, :] == 1)[0]] = np.multiply(deltavalue, DIST[j, np.where(ADJ[j, :] == 1)[0]])
         if j == 1: