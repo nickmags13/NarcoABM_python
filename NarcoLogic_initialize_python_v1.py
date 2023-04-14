corrected latlon2 calculation

NarcoLogic_initialize_python_v1.py
@@ -194,7 +194,6 @@ def NarcoLogic_initialize_python_v1(mr):
     latfac = 1 - nwvec / np.amax(nwvec)
     latfac[0, 0] = 0
 
-    breakpoint()
     # Create adjacency matrix
     iendnode = NodeTable.loc[NodeTable['DeptCode'] == 2, 'ID'].iloc[0]
     ADJ[EdgeTable['EndNodes'].str[0][np.where(EdgeTable['EndNodes'].str[1] == iendnode)[0]], iendnode] = 1
@@ -206,11 +205,13 @@ def NarcoLogic_initialize_python_v1(mr):
         WGHT[0, np.where(ADJ[j, :] == 1)[0]] = EdgeTable['Weight'][np.where(ADJ[j, :] == 1)[0]]
         CPCTY[j, np.where(ADJ[j, :] == 1)[0]] = EdgeTable['Capacity'][np.where(ADJ[j, :] == 1)[0]]
         # Create distance (in km) matrix
-        latlon2 = np.array([NodeTable['Lat'][np.where(ADJ[j, :] == 1)[0]],
-                            NodeTable['Lon'][np.where(ADJ[j, :] == 1)[0]]])
-        latlon1 = np.matlib.repmat(np.array([NodeTable['Lat'][j], NodeTable['Lon'][j]]), len(latlon2[:, 1]), 1)
+        breakpoint()
+        rows = [int(x) for x in range(len(ADJ[j, :])) if ADJ[j, x] == 1]
+        latlon2 = NodeTable.loc[rows, ['Lat', 'Lon']].to_numpy()
+
+        latlon1 = np.tile(np.array([NodeTable['Lat'][j], NodeTable['Lon'][j]]), (len(latlon2[:, 1]), 1))
         d1km, d2km = lldistkm(latlon1, latlon2)
-        DIST[j, ADJ[j, :] == 1] = d1km
+        DIST[j, np.where(ADJ[j, :] == 1)[0]] = d1km
 
         # Create added value matrix (USD) and price per node
         ADDVAL[j, np.where(ADJ[j, :] == 1)[0]] = np.multiply(deltavalue, DIST[j, np.where(ADJ[j, :] == 1)[0]])