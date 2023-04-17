corrected ireceiver calculation

NarcoLogic_initialize_python_v1.py
@@ -242,17 +242,20 @@ def NarcoLogic_initialize_python_v1(mr):
         LATFAC[j, np.where(ADJ[j, :] == 1)[0]] = latfac[np.where(ADJ[j, :] == 1)[0]].flatten()
         BRDRFAC[j, np.where(ADJ[j, :] == 1)[0]] = brdrfac[np.where(ADJ[j, :] == 1)[0]].flatten()
         SUITFAC[j, np.where(ADJ[j, :] == 1)[0]] = suitfac[np.where(ADJ[j, :] == 1)[0]].flatten()
-        breakpoint()
+
         # Transportation costs
-        ireceiver = EdgeTable['EndNodes'].str[1][np.where(EdgeTable['EndNodes'].str[0] == j)[0]]
+        ireceiver = (EdgeTable['EndNodes'].str[1][np.where(EdgeTable['EndNodes'].str[0] == j)[0]]).to_numpy()
+        ireceiver = ireceiver.reshape(len(ireceiver), 1)
         idist_ground = np.logical_and(DIST[j, ireceiver] > 0, DIST[j, ireceiver] <= 500).reshape(-1, 1)
         idist_air = (DIST[j, ireceiver] > 500).reshape(-1, 1)
+        """ CHECK FOR DIVIDE BY ZERO in below 2 lines """
         CTRANS[j, ireceiver[idist_ground], TSTART] = np.multiply(ctrans_inland,
                                                                  DIST[j, ireceiver[idist_ground]]) / DIST[0, mexnode]
         CTRANS[j, ireceiver[idist_air], TSTART] = np.multiply(ctrans_air,
                                                               DIST[j, ireceiver[idist_air]]) / DIST[0, mexnode]
-        if NodeTable['CoastDist'][j] < 20 or 157 <= j <= 159:
-            ireceiver = EdgeTable['EndNodes'].str(1)[EdgeTable['EndNodes'].str(0) == j]
+        breakpoint()
+        if NodeTable.loc[j, 'CoastDist'] < 20 or 156 <= j <= 158:
+            ireceiver = EdgeTable['EndNodes'].str[1][EdgeTable['EndNodes'].str[0] == j]
             idist_coast = (NodeTable['CoastDist'][ireceiver] < 20).reshape(-1, 1)
             idist_inland = (NodeTable['CoastDist'][ireceiver] >= 20).reshape(-1, 1)
             CTRANS[j, ireceiver[idist_coast], TSTART] = np.multiply(ctrans_coast,