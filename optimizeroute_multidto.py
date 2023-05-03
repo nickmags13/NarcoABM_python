68756236fb131b17cd003180cd2977f417989cf0
Karnesh Jain <jain.karnesh@gmail.com>
2023-05-03 14:27:06 -0500

    added edgeparms initialization

--- optimizeroute_multidto.py
+++ optimizeroute_multidto.py
@@ -5,4 +5,9 @@ import numpy as np
 def optimizeroute_multidto(dtorefvec, subflow, supplyfit, expmax, subroutepref, dtoEdgeTable, dtoSLRISK, dtoADDVAL,
                            dtoCTRANS, losstolval, dtoslsuc):
 
+    iactiveedges = np.logical_or(np.where(subflow > 0), np.where(dtoslsuc > 0))
+    actrow = iactiveedges[0]
+    actcol = iactiveedges[1]
+    edgeparms = np.array([subflow[iactiveedges], dtoSLRISK[iactiveedges], iactiveedges, actrow, actcol])
+
     return newroutepref
