61be2a24d427c40145d732163aec7ce2c4d0445d
Karnesh Jain <jain.karnesh@gmail.com>
2023-05-03 13:39:22 -0500

    started optimizeroute_multidto function

---
+++ optimizeroute_multidto.py
@@ -0,0 +1,8 @@
+# Top-down supply chain optimization ########
+
+import numpy as np
+
+def optimizeroute_multidto(dtorefvec, subflow, supplyfit, expmax, subroutepref, dtoEdgeTable, dtoSLRISK, dtoADDVAL,
+                           dtoCTRANS, losstolval, dtoslsuc):
+
+    return newroutepref
