92ccebdc0c3c6b72254b4798619de89649397b46
Karnesh Jain <jain.karnesh@gmail.com>
2023-05-16 10:27:09 -0500

    changed file name and removed extra comments

--- optimize.py
+++ optimize.py
@@ -9,19 +9,6 @@ def optimize_interdiction_batch(ADJ):
 
     trgtfile = 'data/MTMCI_IntNodes.txt'
 
-    """
-    readflag = 0
-    print('Looking for ' + trgtfile)
-
-    fileSearch = trgtfile.split('.')
-    fnames = glob.glob(fileSearch[0]+"*"+fileSearch[1])
-    for file in fnames:
-        if os.path.getsize(trgtfile) > 0:
-            readflag = 1
-
-    print('Interdiction Input File Success, t= ' + t)
-    """
-
     Tintevent = np.loadtxt(trgtfile, dtype=int)
     intrdct_events = np.zeros(ADJ.shape)
     intrdct_nodes = Tintevent
