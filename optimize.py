corrected iprimary calculation

optimize.py
@@ -27,11 +27,11 @@ def optimizeroute_multidto(dtorefvec, subflow, supplyfit, expmax, subroutepref, 
                                    iactiveedges[1][edge]]))
     edgeparms = np.array(edgeparms)
 
-    breakpoint()
     if supplyfit < losstolval:  # need to consolidate supply chain
         edgesort = edgeparms[edgeparms[:, 1].argsort()[::-1]]
         # primary movement
-        iprimary = np.logical_and(np.where(edgesort[:, 3] == 1), np.where(edgesort[:, 4] != len(dtorefvec)))
+        breakpoint()
+        iprimary = np.intersect1d(np.where(edgesort[:, 2] == 0)[0], np.where(edgesort[:, 3] != len(dtorefvec)-1)[0])
         edgecut = np.arange(1, np.amin(np.round(len(iactiveedges) * (supplyfit / (supplyfit + losstolval))),
                                        len(iactiveedges) - 1))
 