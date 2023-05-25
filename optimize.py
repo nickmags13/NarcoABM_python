corrected send_row and rec_col calculation

optimize.py
@@ -115,9 +115,10 @@ def optimizeroute_multidto(dtorefvec, subflow, supplyfit, expmax, subroutepref, 
             rec_col = []
             breakpoint()
             for jj in range(0, len(ireceivers[:, 0])):
-                send_row = np.array([[send_row], [np.where(dtorefvec[np.in1d(dtorefvec, ireceivers[jj, 0])] == 1)]])
-                rec_col = np.array([[rec_col], [np.where(dtorefvec[np.in1d(dtorefvec, ireceivers[jj, 1])] == 1)]])
-            subroutepref[send_row, rec_col] = 1
-            subroutepref[rec_col, len(dtorefvec)] = 1
+                send_row = np.append(send_row, np.where(np.in1d(dtorefvec, ireceivers[jj, 0]) == 1)[0])
+                rec_col = np.append(rec_col, np.where(np.in1d(dtorefvec, ireceivers[jj, 1]) == 1)[0])
+            breakpoint()
+            subroutepref[np.int_(send_row), np.int_(rec_col)] = 1
+            subroutepref[np.int_(rec_col), len(dtorefvec)] = 1
 
     return subroutepref