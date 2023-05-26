corrected inei calculation

narcologic.py
@@ -355,7 +355,6 @@ def main(mr, times):
                 rtdto = NodeTable.loc[np.where(ADJ[n, :] == 1)[0], 'DTO']
                 if len(np.where(rtdto == 0)[0]) > 0:
                     rtdto[(np.where[rtdto] == 0)[0]] = NodeTable.loc[n, 'DTO']
-                breakpoint()
                 CPCTY[n, np.where(ADJ[n, :] == 1)[0]] = basecap[0, erun] * rtcap[
                     rtdto - 1, int(np.floor(time / 12)) + 1]
                 TOTCPTL[n, time] = TOTCPTL[n, time - 1] + TOTCPTL[n, time]
@@ -364,7 +363,9 @@ def main(mr, times):
                         LEAK[n, time] = nodeloss * STOCK[n, time]
                         STOCK[n, time] = STOCK[n, time] - LEAK[n, time]
                     elif n == 0:
-                        inei = np.logical_and(np.where(ADJ[n, :] == 1), np.where(routepref[n, :, time] > 0))
+                        inei = np.intersect1d(np.where(ADJ[n, :] == 1),
+                                              np.where(routepref[n, :, time] > 0)).reshape(1, -1)
+                        breakpoint()
                         for nd in range(0, len(np.unique(NodeTable.loc[1:nnodes, 'DTO']))):
                             if len(np.where(NodeTable.loc[inei, 'DTO'] == nd)) == 0:
                                 idtombr = np.where(NodeTable['DTO'] == nd)